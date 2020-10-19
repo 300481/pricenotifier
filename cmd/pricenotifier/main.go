5363f5c5bd63081c836e2d00612f09f9635cb39e
300481 <300481@300481.de>
2020-10-19 15:24:29 +0200

    remove E10, send notification only for the best price station

--- cmd/pricenotifier/main.go
+++ cmd/pricenotifier/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/300481/pricenotifier/pkg/persistence"
 	"github.com/300481/pricenotifier/pkg/pricehistory"
 	"github.com/300481/pricenotifier/pkg/pricesource"
+	"github.com/alexruf/tankerkoenig-go"
 )
 
 func main() {
@@ -33,15 +34,12 @@ func main() {
 
 	ts := time.Now().Unix()
 
-	var e5, e10, diesel []float64
+	var e5, diesel []float64
 
 	for _, station := range stations {
 		if station.IsOpen {
 			if station.E5 != nil {
 				e5 = append(e5, station.E5.(float64))
-			}
-			if station.E10 != nil {
-				e10 = append(e10, station.E10.(float64))
 			}
 			if station.Diesel != nil {
 				diesel = append(diesel, station.Diesel.(float64))
@@ -50,11 +48,9 @@ func main() {
 	}
 
 	ph.AddFuelPrices(pricehistory.Timestamp(ts), pricehistory.E5, e5)
-	ph.AddFuelPrices(pricehistory.Timestamp(ts), pricehistory.E10, e10)
 	ph.AddFuelPrices(pricehistory.Timestamp(ts), pricehistory.Diesel, diesel)
 
 	ph.CleanHistory(pricehistory.E5)
-	ph.CleanHistory(pricehistory.E10)
 	ph.CleanHistory(pricehistory.Diesel)
 
 	w := gcs.NewWriter()
@@ -63,25 +59,37 @@ func main() {
 		fmt.Println("error closing persistence")
 	}
 
+	var goodStationE5 tankerkoenig.Station
+	var goodStationDiesel tankerkoenig.Station
+
+	goodStationE5.E5 = 20.00
+	goodStationDiesel.Diesel = 20.00
+
 	for _, station := range stations {
 		if station.IsOpen {
 			if station.E5 != nil {
 				if ph.GoodPrice(pricehistory.E5, station.E5.(float64)) {
-					notify.Notify(station, pricehistory.E5)
-					log.Printf("found good price for E5: %.3f€ at %s %s", station.E5.(float64), station.Brand, station.Place)
+					if station.E5.(float64) < goodStationE5.E5.(float64) {
+						goodStationE5 = station
+					}
 				}
 			}
-			// if station.E10 != nil {
-			// 	if ph.GoodPrice(pricehistory.E10, station.E10.(float64)) {
-			// 		notify.Notify(station, pricehistory.E10)
-			// 	}
-			// }
 			if station.Diesel != nil {
 				if ph.GoodPrice(pricehistory.Diesel, station.Diesel.(float64)) {
-					notify.Notify(station, pricehistory.Diesel)
-					log.Printf("found good price for Diesel: %.3f€ at %s %s", station.Diesel.(float64), station.Brand, station.Place)
+					if station.Diesel.(float64) < goodStationDiesel.Diesel.(float64) {
+						goodStationDiesel = station
+					}
 				}
 			}
 		}
+	}
+
+	if goodStationE5.E5.(float64) < 20.00 {
+		notify.Notify(goodStationE5, pricehistory.E5)
+		log.Printf("found good price for E5: %.3f€ at %s %s", goodStationE5.E5.(float64), goodStationE5.Brand, goodStationE5.Place)
+	}
+	if goodStationDiesel.Diesel.(float64) < 20.00 {
+		notify.Notify(goodStationDiesel, pricehistory.Diesel)
+		log.Printf("found good price for Diesel: %.3f€ at %s %s", goodStationDiesel.Diesel.(float64), goodStationDiesel.Brand, goodStationDiesel.Place)
 	}
 }
