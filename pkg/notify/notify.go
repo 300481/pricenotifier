ca58ba2fc21363a884229ea52bd220e7a89f2a4a
300481 <300481@300481.de>
2020-11-03 21:51:02 +0100

    refactor notify

--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -6,19 +6,117 @@ import (
 	"os"
 
 	"github.com/300481/pricenotifier/pkg/pricehistory"
+	"github.com/300481/pricenotifier/pkg/station"
 	"github.com/alexruf/tankerkoenig-go"
 	"github.com/gregdel/pushover"
 )
 
-// Notify will send a notification via pushover
-func Notify(station tankerkoenig.Station, fuel pricehistory.Fuel) {
-	token := os.Getenv("PUSHOVER_TOKEN")
-	user := os.Getenv("PUSHOVER_USER")
+type fueltype string
+type stationID string
+
+// Notifier represents a notifier struct with information about best stations and last notifications
+type Notifier struct {
+	currentBestStations  map[fueltype]station.StationMap
+	currentBestPrices    map[fueltype]map[stationID]float64
+	notifiedBestStations map[fueltype]station.StationMap
+	notifiedBestPrices   map[fueltype]map[stationID]float64
+	client               Client
+}
+
+// NewNotifier returns an initialized *Notifier
+func NewNotifier(client Client) *Notifier {
+	return &Notifier{
+		currentBestStations:  make(map[fueltype]station.StationMap),
+		currentBestPrices:    make(map[fueltype]map[stationID]float64),
+		notifiedBestStations: make(map[fueltype]station.StationMap),
+		notifiedBestPrices:   make(map[fueltype]map[stationID]float64),
+		client:               client,
+	}
+}
+
+// UpdateBestStations updates the best stations of the Notifier
+func (n *Notifier) UpdateBestStations(fuel string, bestStations station.StationMap) {
+	n.currentBestStations[fueltype(fuel)] = bestStations
+
+	// make prices map if needed
+	if _, ok := n.currentBestPrices[fueltype(fuel)]; !ok {
+		n.currentBestPrices[fueltype(fuel)] = make(map[stationID]float64)
+	}
+
+	// cleanup prices map
+	for ID := range n.currentBestPrices[fueltype(fuel)] {
+		delete(n.currentBestPrices[fueltype(fuel)], ID)
+	}
+
+	// save current best prices
+	for ID, s := range n.currentBestStations[fueltype(fuel)] {
+		n.currentBestPrices[fueltype(fuel)][stationID(ID)] = s.LatestPrice(fuel)
+	}
+}
+
+// Notify will send a notification if there are best stations available currently
+// Returns if a message was send
+func (n *Notifier) Notify() bool {
+	var msg string
+	// for each fuel
+	for fuel, sm := range n.currentBestStations {
+		// for each station of the best stations
+		for ID, s := range sm {
+			price := n.currentBestPrices[fuel][stationID(ID)]
+			msg += fmt.Sprintf(
+				"Best price for %s : %.3f€ \nat %s \nin %s\n\n",
+				string(fuel), price, s.Brand(), s.Place(),
+			)
+		}
+	}
+	// if there is a best station
+	if len(msg) > 0 {
+		return n.client.Notify(msg)
+	}
+	return false
+}
+
+// Client is an interface for a notification client
+type Client interface {
+	Notify(message string) bool
+}
+
+// pushoverClient represents a notification client for Pushover
+type pushoverClient struct {
+	token     string
+	user      string
+	app       *pushover.Pushover
+	recipient *pushover.Recipient
+}
+
+// NewPushover returns a client interface for a pushover client
+func NewPushover(token, user string) Client {
 	app := pushover.New(token)
 	recipient := pushover.NewRecipient(user)
+	return &pushoverClient{
+		token:     token,
+		user:      user,
+		app:       app,
+		recipient: recipient,
+	}
+}
 
+// Notify will send a notification via Pushover
+// returns if the message was send without failure
+func (p *pushoverClient) Notify(message string) bool {
+	msg := pushover.NewMessage(message)
+	response, err := p.app.SendMessage(msg, p.recipient)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println(response)
+	return true
+}
+
+// Notify will send a notification via pushover
+func Notify(station tankerkoenig.Station, fuel pricehistory.Fuel) {
 	var msg string
-
 	switch fuel {
 	case "Diesel":
 		msg = fmt.Sprintf("Price for Diesel: %.3f€ \nat %s \nin %s", station.Diesel.(float64), station.Brand, station.Place)
@@ -27,11 +125,8 @@ func Notify(station tankerkoenig.Station, fuel pricehistory.Fuel) {
 	case "E5":
 		msg = fmt.Sprintf("Price for E5: %.3f€ \nat %s \nin %s", station.E5.(float64), station.Brand, station.Place)
 	}
-
-	message := pushover.NewMessage(msg)
-	response, err := app.SendMessage(message, recipient)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(response)
+	NewPushover(
+		os.Getenv("PUSHOVER_TOKEN"),
+		os.Getenv("PUSHOVER_USER"),
+	).Notify(msg)
 }
---
+++ pkg/notify/notify_test.go
@@ -0,0 +1,64 @@
+package notify
+
+import (
+	"testing"
+
+	"github.com/300481/pricenotifier/pkg/station"
+)
+
+type testClient struct{}
+
+func TestUpdateBestStations(t *testing.T) {
+	price := float64(1.0)
+	stations := []string{"A", "B", "C"}
+	fuels := []string{"E5", "Diesel"}
+
+	n := NewNotifier(&testClient{})
+
+	for _, fuel := range fuels {
+		sm := station.NewStationMap()
+		for _, s := range stations {
+			sm[s] = station.NewStation(s, s, s)
+			sm[s].AddPrice(1, fuel, price)
+		}
+		n.UpdateBestStations(fuel, sm)
+
+		nStations := len(stations)
+
+		nBestStations := len(n.currentBestStations[fueltype(fuel)])
+		if nBestStations != nStations {
+			t.Errorf("Count of best stations don't fits the right size. Got: %d must: %d", nBestStations, nStations)
+		}
+
+		nBestPrices := len(n.currentBestPrices[fueltype(fuel)])
+		if nBestPrices != nStations {
+			t.Errorf("Count of best prices don't fits the right size. Got: %d must: %d", nBestPrices, nStations)
+		}
+	}
+}
+
+// TestNotify tests the notifiers notify function
+func TestNotify(t *testing.T) {
+	testPrice := float64(1.0)
+	testFuel := "Diesel"
+	testStation := "A"
+
+	n := NewNotifier(&testClient{})
+
+	sm := station.NewStationMap()
+	sm[testStation] = station.NewStation(testStation, testStation, testStation)
+	sm[testStation].AddPrice(1, testFuel, testPrice)
+
+	n.UpdateBestStations(testFuel, sm)
+
+	// will call the testclient, which checks if the notification message is ok
+	if !n.Notify() {
+		t.Error("Test Notify failed. Notification string not as expected.")
+	}
+}
+
+// Notify is the testClient notification function which tests if the message is ok
+func (t *testClient) Notify(message string) bool {
+	testMessage := "Best price for Diesel : 1.000€ \nat A \nin A\n\n"
+	return message == testMessage
+}
