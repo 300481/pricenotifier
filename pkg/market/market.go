a40124a15bd6c3d602aa1a9aed4a66331ca9d4e4
300481 <300481@300481.de>
2020-11-04 21:28:17 +0100

    export market fields to provide marshalling

--- pkg/market/market.go
+++ pkg/market/market.go
@@ -13,13 +13,13 @@ import (
 
 // Market represents the whole station market
 type Market struct {
-	station station.StationMap // map of station IDs and stations
+	Station station.StationMap // map of station IDs and stations
 }
 
 // NewMarket returns an initialized *Market
 func NewMarket() *Market {
 	m := &Market{
-		station: station.NewStationMap(),
+		Station: station.NewStationMap(),
 	}
 	return m
 }
@@ -32,7 +32,7 @@ func (m *Market) BestStations(fuel string) station.StationMap {
 	mean, dev := stat.MeanStdDev(prices, nil)
 	goodPrice := mean - dev
 
-	for ID, s := range m.station {
+	for ID, s := range m.Station {
 		latestPrice := s.LatestPrice(fuel)
 		if latestPrice < goodPrice {
 			bestStations[ID] = s
@@ -45,7 +45,7 @@ func (m *Market) BestStations(fuel string) station.StationMap {
 // allPrices returns all prices for a fuel type
 func (m *Market) allPrices(fuel string) []float64 {
 	var prices []float64
-	for _, s := range m.station {
+	for _, s := range m.Station {
 		stationPrices := s.PricesSince(maxAge, fuel)
 		prices = append(prices, stationPrices...)
 	}
@@ -55,8 +55,8 @@ func (m *Market) allPrices(fuel string) []float64 {
 
 // UpsertStation upserts a station to the market
 func (m *Market) UpsertStation(ID, brand, name, place string) {
-	if s, ok := m.station[ID]; !ok {
-		m.station[ID] = station.NewStation(brand, name, place)
+	if s, ok := m.Station[ID]; !ok {
+		m.Station[ID] = station.NewStation(brand, name, place)
 	} else {
 		s.Update(brand, name, place)
 	}
@@ -64,11 +64,11 @@ func (m *Market) UpsertStation(ID, brand, name, place string) {
 
 // AddPrice adds a fuel price for a station and the given Unix epoch timestamp
 func (m *Market) AddPrice(timestamp int64, ID, fuel string, price float64) error {
-	if _, ok := m.station[ID]; !ok {
+	if _, ok := m.Station[ID]; !ok {
 		err := fmt.Sprintf("Station with ID: %s not existing.", ID)
 		return errors.New(err)
 	}
-	m.station[ID].AddPrice(timestamp, fuel, price)
+	m.Station[ID].AddPrice(timestamp, fuel, price)
 	return nil
 }
 
