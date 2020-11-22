890f81c83ce1edc6ee8efbf1e51c685fb52af597
300481 <300481@300481.de>
2020-11-22 15:06:07 +0100

    extend Market interface with Get function, add GetOption

--- pkg/market/market.go
+++ pkg/market/market.go
@@ -6,10 +6,21 @@ import "time"
 type Market interface {
 	Add(station *Station)                                                         // Add a station information to the market
 	GetBestPriceStations(customer *Customer, fuelType FuelType) (Stations, error) // Get the best price stations of interest for a customer and fuel type
+	Get(customer *Customer, option GetOption)
 }
 
 // FuelType is a type representing a fuel
 type FuelType string
+
+// GetOption defines the range of returned stations for the Get function
+type GetOption string
+
+const (
+	// GetAll returns all stations of customer interest
+	GetAll GetOption = "all"
+	// GetBest returns only the stations of customer interest with the best price
+	GetBest GetOption = "best"
+)
 
 // Station represents station information
 type Station struct {
