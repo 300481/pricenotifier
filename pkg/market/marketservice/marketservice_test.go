33b33f3138edcbbc908b0e220bbaff251b0c0c49
300481 <300481@300481.de>
2020-11-26 22:58:34 +0100

    replace timestamp by time.Time, add test for get best stations

--- pkg/market/marketservice/marketservice_test.go
+++ pkg/market/marketservice/marketservice_test.go
@@ -3,7 +3,9 @@ package marketservice
 import (
 	"testing"
 
+	"github.com/300481/pricenotifier/pkg/market"
 	"github.com/300481/pricenotifier/pkg/market/mock"
+	"gonum.org/v1/gonum/stat"
 )
 
 func TestAdd(t *testing.T) {
@@ -31,7 +33,7 @@ func TestAdd(t *testing.T) {
 		if station.Lng != mstation.Lng {
 			t.Errorf("Lng for Station wrong. ID: %s", station.ID)
 		}
-		ts := Timestamp(station.Timestamp.Unix())
+		ts := station.Timestamp
 		if station.Price["Diesel"] != mstation.Price["Diesel"][ts] {
 			t.Errorf("Price Diesel for Station wrong. ID: %s", station.ID)
 		}
@@ -48,26 +50,63 @@ func TestGet(t *testing.T) {
 	stations := mock.Stations()
 	m := NewMarketService()
 	c := mock.Customer()
+	prices := make(map[market.FuelType][]float64)
+	goodPrice := make(map[market.FuelType]float64)
 
+	// add station to market, fill prices slice
 	for _, station := range stations {
 		m.Add(station)
+		for fuelType, price := range station.Price {
+			prices[fuelType] = append(prices[fuelType], price)
+		}
 	}
 
-	gstations := m.Get(c, "all")
+	// test get all stations
+	getStations := m.Get(c, "all")
 
 	for _, fuelType := range c.Fuels {
 		for _, src := range stations {
 			found := false
-			for _, dst := range gstations[fuelType] {
+			for _, dst := range getStations[fuelType] {
 				if dst.ID == src.ID {
 					found = true
 				}
 			}
 			if !found {
-				t.Errorf("Station not got back for fuel %s and for ID %s", string(fuelType), src.ID)
+				t.Errorf("Station %s not got back for fuel %s and for ID %s", src.Brand, string(fuelType), src.ID)
 			}
 		}
 	}
 
-	// TODO implement test for "best"
+	// calculate good price
+	for fuelType, fuelPrices := range prices {
+		mean, dev := stat.MeanStdDev(fuelPrices, nil)
+		goodPrice[fuelType] = mean - dev
+	}
+
+	// find good stations for the fuels
+	goodStations := make(map[market.FuelType]market.Stations)
+	for _, fuelType := range c.Fuels {
+		for _, station := range stations {
+			if station.Price[fuelType] <= goodPrice[fuelType] {
+				goodStations[fuelType] = append(goodStations[fuelType], station)
+			}
+		}
+	}
+
+	// test get best stations
+	getStations = m.Get(c, "best")
+	for _, fuelType := range c.Fuels {
+		for _, src := range goodStations[fuelType] {
+			found := false
+			for _, dst := range getStations[fuelType] {
+				if dst.ID == src.ID {
+					found = true
+				}
+			}
+			if !found {
+				t.Errorf("Station %s not got back for fuel %s and for ID %s price %.3f good price %.3f", src.Brand, string(fuelType), src.ID, src.Price[fuelType], goodPrice[fuelType])
+			}
+		}
+	}
 }
