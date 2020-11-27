92f7bc760b35a0899c8caa1c8f27decc0d9d2ac9
300481 <300481@300481.de>
2020-11-27 06:33:21 +0100

    let pricesource only define the interface

--- pkg/pricesource/pricesource.go
+++ pkg/pricesource/pricesource.go
@@ -1,38 +1,10 @@
 package pricesource
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-
-	"github.com/alexruf/tankerkoenig-go"
+	"github.com/300481/pricenotifier/pkg/market"
 )
 
-// GetStations returns a list of tankerkoenig Station
-func GetStations() ([]tankerkoenig.Station, error) {
-	apiKey := os.Getenv("API_KEY")
-
-	lat, err := strconv.ParseFloat(os.Getenv("LAT"), 64)
-	if err != nil {
-		fmt.Println("error converting LAT to float", err)
-	}
-
-	lng, err := strconv.ParseFloat(os.Getenv("LNG"), 64)
-	if err != nil {
-		fmt.Println("error converting LNG to float", err)
-	}
-
-	rad, err := strconv.Atoi(os.Getenv("RAD"))
-	if err != nil {
-		fmt.Println("error converting RAD to integer", err)
-	}
-
-	client := tankerkoenig.NewClient(apiKey, nil)
-	stations, _, err := client.Station.List(lat, lng, rad)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return stations, nil
+// PriceSource defines the interface of a PriceSource
+type PriceSource interface {
+	GetStations() (market.Stations, error)
 }
